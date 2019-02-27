260e0646fe52fc429b18a607d25c7cdb7da53ff1
Ben Vineyard <ben.vineyard@kaseya.com>
2019-02-27 14:51:17 -0800

    Initial release

---
+++ src/config/index.js
@@ -0,0 +1,69 @@
+module.exports = {
+  siteTitle: 'Ben Vineyard | Principal Fullstack Engineer',
+  siteDescription:
+    'Ben Vineyard is a Principal Fullstack Engineer based in Las Vegas, NV who specializes in developing (and occasionally designing) exceptional, high-quality websites and applications.',
+  siteKeywords:
+    'Ben Lynn Vineyard IV, Ben Lynn Vineyard, Ben Vineyard IV, Ben, Vineyard, benvineyard, software engineer, front-end engineer, web developer, javascript, las vegas, typescript, nodejs, c#, asp.net, .net',
+  siteUrl: 'http://benvineyard.com',
+  siteLanguage: 'en_US',
+
+  name: 'Ben Vineyard',
+  location: 'Las Vegas, NV',
+  email: 'ben@benvineyard.com',
+  socialMedia: [
+    {
+      name: 'Github',
+      url: 'https://github.com/benvineyard/',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/benvineyard/',
+    },
+  ],
+
+  nav: [
+    {
+      name: 'About',
+      url: '#about',
+    },
+    {
+      name: 'Experience',
+      url: '#jobs',
+    },
+    {
+      name: 'Work',
+      url: '#projects',
+    },
+    {
+      name: 'Contact',
+      url: '#contact',
+    },
+  ],
+
+  twitterHandle: '@bchiang7',
+  googleAnalyticsID: 'UA-45666519-2',
+
+  headerHeight: 100,
+
+  greenColor: '#64ffda',
+  navyColor: '#0a192f',
+  darkNavyColor: '#020c1b',
+
+  srConfig: (delay = 200) => {
+    return {
+      origin: 'bottom',
+      distance: '20px',
+      duration: 500,
+      delay,
+      rotate: { x: 0, y: 0, z: 0 },
+      opacity: 0,
+      scale: 1,
+      easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+      mobile: true,
+      reset: false,
+      useDelay: 'always',
+      viewFactor: 0.25,
+      viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+    };
+  },
+};
