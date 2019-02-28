d2c60f9d26b391dd807f588538167b952e95b55d
Ben Vineyard <ben.vineyard@kaseya.com>
2019-02-28 08:39:42 -0800

    chore: updating resume link

--- src/components/header.js
+++ src/components/header.js
@@ -301,7 +301,7 @@ class Header extends Component {
               {isMounted && (
                 <CSSTransition classNames="fadedown" timeout={3000}>
                   <div style={{ transitionDelay: `600ms` }}>
-                    <ResumeLink href={resume} target="_blank" rel="nofollow noopener noreferrer">
+                    <ResumeLink href="resume.pdf" target="_blank" rel="nofollow noopener noreferrer">
                       Resume
                     </ResumeLink>
                   </div>
